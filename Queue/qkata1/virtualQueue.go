b8d70a83af6e8cbdf3db626fff2ce5497eef7dd1
jduda27 <joshua.duda@gmail.com>
2024-06-20 15:05:18 -0500

    Virtual Queue implemented and test conditions validated

--- Queue/qkata1/virtualQueue.go
+++ Queue/qkata1/virtualQueue.go
@@ -1,26 +1,93 @@
 package qkata1
 
+import (
+	"fmt"
+)
+
 type Visitor struct {
-  visID string
-  next *Visitor
+	visID string
+	next  *Visitor
 }
 
 type Queue struct {
-  head *Visitor
+	head *Visitor
 }
 
+type VisIDError struct{ visID string }
+type QueueEmptyError struct{}
 
+func (m *VisIDError) Error() string {
+	return "Visitor ID " + m.visID + " not found"
+}
 
-func (q Queue) EnterQueue (v string) {
-  visitor := &Visitor{visID:v ,next:nil }
+func (m *QueueEmptyError) Error() string {
+	return "Queue is empty."
+}
 
-  if q.head == nil {
-     q.head = visitor
-  } else {
-    p := q.head
-    for p.next != nil {
-      p = p.next
-    }
-    p.next = visitor
-  }
+func (q *Queue) EnterQueue(v string) {
+	visitor := &Visitor{visID: v, next: nil}
+
+	if q.head == nil {
+		q.head = visitor
+	} else {
+		p := q.head
+		for p.next != nil {
+			p = p.next
+		}
+		p.next = visitor
+	}
+}
+
+func (q *Queue) ExitQueue(target string) (err error) {
+	err = nil
+	tf := false
+	p := q.head
+	var visitorBefore, visitorAfter *Visitor
+
+	for p != nil {
+		if p.visID == target {
+			tf = true
+			visitorAfter = p.next
+			break
+		}
+		if p.next != nil && p.next.visID == target {
+			visitorBefore = p
+		}
+
+		p = p.next
+	}
+
+	if !tf {
+		err = &VisIDError{visID: target}
+	} else {
+		if visitorBefore == nil {
+			q.head = q.head.next
+		} else {
+			visitorBefore.next = visitorAfter
+		}
+	}
+
+	return err
+}
+
+func (q *Queue) RemoveNextInQueue() {
+	var err error
+	if q.head != nil {
+		p := q.head.visID
+		err = q.ExitQueue(p)
+	} else {
+		err = &QueueEmptyError{}
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Show(q Queue) {
+	p := q.head
+	for p != nil {
+		fmt.Printf("%v -> ", p.visID)
+		p = p.next
+	}
+	fmt.Println()
 }
--- Queue/qkata1/virtualQueue_test.go
+++ Queue/qkata1/virtualQueue_test.go
@@ -1,13 +1,72 @@
 package qkata1
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestAddToQueue(t *testing.T) {
 
+	fmt.Println("Test add to queue")
 	queue := Queue{}
 	queue.EnterQueue("000001")
 	queue.EnterQueue("000002")
+	queue.EnterQueue("000003")
+
+	Show(queue)
+
+}
+
+func TestRemoveNextInQueue(t *testing.T) {
+
+	fmt.Println("Test remove next in queue")
+	queue := Queue{}
+	queue.EnterQueue("000001")
+	queue.EnterQueue("000002")
+	queue.EnterQueue("000003")
+	queue.EnterQueue("000004")
+
+	Show(queue)
+	queue.RemoveNextInQueue()
+	Show(queue)
+	queue.RemoveNextInQueue()
+	Show(queue)
+	queue.RemoveNextInQueue()
+	Show(queue)
+	queue.RemoveNextInQueue()
+	Show(queue)
+	queue.RemoveNextInQueue()
+}
+
+func TestExitQueue(t *testing.T) {
+
+	var err error
+	fmt.Println("Test remove from queue")
+	queue := Queue{}
+	queue.EnterQueue("000001")
+	queue.EnterQueue("000002")
+	queue.EnterQueue("000003")
+	queue.EnterQueue("000004")
+
+	Show(queue)
+	err = queue.ExitQueue("000002")
+	if err != nil {
+		fmt.Println(err)
+	}
+	Show(queue)
+	err = queue.ExitQueue("000001")
+	if err != nil {
+		fmt.Println(err)
+	}
+	Show(queue)
+	err = queue.ExitQueue("000004")
+	if err != nil {
+		fmt.Println(err)
+	}
+	Show(queue)
+	err = queue.ExitQueue("000005")
+	if err != nil {
+		fmt.Println(err)
+	}
 
 }
